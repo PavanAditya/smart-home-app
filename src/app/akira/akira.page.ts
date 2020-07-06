d4b85f7a21e97a74a2bd661aa8fb3f034beeeeb8
Pavan Aditya M S <pavanaditya.ms@stupify.in>
2020-07-06 13:06:37 +0530

    speech recogniton model added to akira page

--- src/app/akira/akira.page.ts
+++ src/app/akira/akira.page.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { GlobalService } from '../global.service';
+import { Platform } from '@ionic/angular';
+
+declare var SpeechRecognition: any;
 
 @Component({
   selector: 'app-akira',
@@ -9,15 +12,41 @@ import { GlobalService } from '../global.service';
 export class AkiraPage implements OnInit {
 
   public theme = 'dark';
+  public isListening = true;
+  public recognizedText: string;
+  public recognition: any;
 
   constructor(
-    private globalService: GlobalService
+    private globalService: GlobalService,
+    public platform: Platform
   ) { }
 
   ngOnInit() {
     this.globalService.theme.subscribe(resp => {
       this.theme = resp;
     });
+  }
+
+  public startSpeechRecognition(): void {
+    this.isListening = !this.isListening;
+    if (this.isListening) {
+      this.platform.ready().then(() => {
+        this.recognition = new SpeechRecognition();
+        this.recognition.lang = 'en-US';
+        this.recognition.onnomatch = (event => {
+          console.log('No match found.');
+        });
+        this.recognition.onerror = (event => {
+          console.log('Error happens.');
+        });
+        this.recognition.onresult = (event => {
+          if (event.results.length > 0) {
+            console.log('Output STT: ', event.results[0][0].transcript);
+          }
+        });
+        this.recognition.start();
+      });
+    }
   }
 
 }
